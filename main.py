3c440b2568115665113597625f2aff584ed4b0f0
Valdirjunior12 <66043046+ValdirJunior13@users.noreply.github.com>
2022-10-16 18:34:07 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+def maquina(limit):
+  if(len(limit > 5)):
+    return "Error: Too many problems"
+    
+  first = ""
+  second = ""
+  third = ""
+  forth = ""
+  string = ""
+  
+  for limits in limit:
+    limits_String = limits.split
+    length = 0
+
+  for max in limits:
+    if len(limits_String) > length:
+      lenght = len(max)
+
+  if lenght > 4:
+    return "Error: Numbers cannot be more than four digits."
+
+  
+
+    if third == "+":
+      result = str(a + b)
+
+    elif third == "-":
+      result = str(a-b)
+    else: 
+      return "Error: Operator must be '+' or '-'."
+
+  if not first.isdigit():
+    return "Error: Numbers must only contain digits."
+  elif not second.isdigit():
+    return "Error: Numbers must only contain digits."
+
+  
+    
+
+ 
+
+
+  
