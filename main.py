f6ce712af6d40b0df71ae02712420ce74267311c
Valdirjunior12 <66043046+ValdirJunior13@users.noreply.github.com>
2022-10-26 20:42:49 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,11 @@
-def maquina(limit):
-  if(len(limit > 5)):
-    return "Error: Too many problems"
-    
-  first = ""
-  second = ""
-  third = ""
-  forth = ""
-  string = ""
-  
-  for limits in limit:
-    limits_String = limits.split
-    length = 0
+# This entrypoint file to be used in development. Start by reading README.md
+from pytest import main
 
-  for max in limits:
-    if len(limits_String) > length:
-      lenght = len(max)
-
-  if lenght > 4:
-    return "Error: Numbers cannot be more than four digits."
-
-  
-
-    if third == "+":
-      result = str(a + b)
-
-    elif third == "-":
-      result = str(a-b)
-    else: 
-      return "Error: Operator must be '+' or '-'."
-
-  if not first.isdigit():
-    return "Error: Numbers must only contain digits."
-  elif not second.isdigit():
-    return "Error: Numbers must only contain digits."
-
-  
-    
-
- 
+from arithmetic_arranger import arithmetic_arranger
 
 
-  
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+
+
+# Run unit tests automatically
+main(['-vv'])
